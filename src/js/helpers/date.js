3a59103b140419e9624aba7d60bc4bba19f534de
Dmitry Kazantsev <mrduk97@gmail.com>
2022-02-14 18:04:43 +0700

    added form validation

--- src/js/helpers/date.js
+++ src/js/helpers/date.js
@@ -8,8 +8,6 @@ export function formateDate(str, type) {
 export function compareDates(depart_date, return_date) {
   const parsedDepartDate = parseISO(depart_date);
   const parsedReturnDate = parseISO(return_date);
-  // console.log(parsedDepartDate, parsedReturnDate);
-  // const departFormatedDate = format(parsedDepartDate, `yyyy M d`);
-  // const returnFormatedDate = format(parsedReturnDate, `yyyy M d`);
+
   return compareAsc(parsedDepartDate, parsedReturnDate);
 }
